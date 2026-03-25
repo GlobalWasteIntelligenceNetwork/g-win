ce2f366a6307dcd7ed8d34769a62c494599278f2
GlobalWasteIntelligenceNetwork <yaani.maa@gmail.com>
2026-03-25 03:51:03 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -48,3 +48,12 @@ window.login = function() {
       alert("Error: " + error.message);
     });
 }
+
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if true;
+    }
+  }
+}
