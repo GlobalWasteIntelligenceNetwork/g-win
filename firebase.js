f3ecb34cc9b25515edb7ebe3f78eb797bd9a2737
GlobalWasteIntelligenceNetwork <yaani.maa@gmail.com>
2026-03-25 02:53:35 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,19 @@
-// Import Firebase modular SDK
+//
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
-// Your Firebase config
+//
 const firebaseConfig = {
   apiKey: "AIzaSyD2zv3-hzcb3RiqYDhbZAEnj1Spka9ONmw",
   authDomain: "g-win-3e858.firebaseapp.com",
   projectId: "g-win-3e858"
 };
 
-// Initialize Firebase
+//
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// ---------------- SIGN UP ----------------
+----------------
 window.signUp = function() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
@@ -31,7 +31,7 @@ window.signUp = function() {
     });
 }
 
-// ---------------- LOGIN ----------------
+----------------
 window.login = function() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
